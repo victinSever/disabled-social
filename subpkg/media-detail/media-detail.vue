e3a6f5f5f0cee9099a459a8578ce72c589fdd343
Mr.Liu <773890440@qq.com>
2022-08-03 22:55:41 +0800

    1.0.3

---
+++ subpkg/media-detail/media-detail.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<mover></mover>
+		
+		<view class="">
+			1231
+		</view>
+		<view class="media-footer">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
