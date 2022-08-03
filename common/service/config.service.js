35188a2fcd4bd4647fb6db08b0e01e1f3b36d979
wzq <1908874993@qq.com>
2022-08-03 14:53:14 +0800

    地址

--- common/service/config.service.js
+++ common/service/config.service.js
@@ -1,11 +1,8 @@
 let BASE_URL = ''
 
 
-if (process.env.NODE_ENV == 'development') {
-    BASE_URL = 'https://stage.du-power.com' // 开发环境
-} else {
-	BASE_URL = '' // 生产环境
-}
+BASE_URL = ''
+
 let staticDomainURL = BASE_URL+ '/sys/common/static';//预留图片路径
 
 const configService = {
