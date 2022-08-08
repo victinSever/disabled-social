fd44b1dc3efa845f3d8006f913b2ab1fa0b787f5
wzq <1908874993@qq.com>
2022-08-08 14:51:44 +0800

    上拉加载

--- components/social-concern/social-concern.vue
+++ components/social-concern/social-concern.vue
@@ -36,7 +36,7 @@
 					use: true,
 					auto: false,
 					page: {
-						num: 2,
+						num: 0,
 						size: 10
 					},
 					noMoreSize: 10,
@@ -57,25 +57,7 @@
 				this.$emit('init', mescroll)
 			},
 			downCallback(e) {
-				console.log(e)
-				let _that = this;
-				if (_that.flag == true) {
-					around.getRecomment({
-						page: e.num,
-						size: 10,
-						userId: 1
-					}).then(res => {
-						_that.acitveData.unshift(...res.data)
-						if (res.data.length < 10) {
-							_that.flag = false
-							_that.mescroll.optDown.textSuccess = '暂无更多数据'
-						}
-						_that.mescroll.endByPage(res.data.length, 10000);
-					})
-				} else {
-					// _that.mescroll.endByPage(1, 10000);
-				}
-
+				this.mescroll.resetUpScroll();
 			},
 			upCallback(e) {
 				let _that = this;
@@ -84,8 +66,6 @@
 					size: 10,
 					userId: 1
 				}).then(res => {
-					_that.page++
-					console.log(res.data);
 					_that.acitveData.push(...res.data)
 					if (res.data.length < 10)
 						_that.flag = false
