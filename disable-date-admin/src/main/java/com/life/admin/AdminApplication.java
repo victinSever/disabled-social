0931c7bd7414792ca9bee207add7d5104830c976
Chunming Liu <liuchunming@codingforjoy.com>
2022-08-14 16:27:38 +0800

    fixed 冲突

--- disable-date-admin/src/main/java/com/life/admin/AdminApplication.java
+++ disable-date-admin/src/main/java/com/life/admin/AdminApplication.java
@@ -3,6 +3,7 @@ package com.life.admin;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,23 +13,24 @@ import org.springframework.web.client.RestTemplate;
 public class AdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class,args);
+        SpringApplication.run(AdminApplication.class, args);
     }
 
     @Bean
-    public RestHighLevelClient client(){
+    public RestHighLevelClient client(@Value("${es.url}") String esUrl) {
         return new RestHighLevelClient(RestClient.builder(
 //                HttpHost.create("http://175.178.177.105:9200")
-                HttpHost.create("http://222.177.66.230:9200")
+                HttpHost.create(esUrl)
         ));
     }
 
     /**
      * 创建RestTemplate 并注入Spring容器
+     *
      * @return
      */
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
